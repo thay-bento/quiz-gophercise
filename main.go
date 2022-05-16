6f5b6f7b24c3b0c2ae7d6c6dfd4a62cd6b60e342
thay-bento <thayna.silva@stone.com.br>
2022-05-16 11:33:29 -0300

    Update 2

--- main.go
+++ main.go
@@ -7,25 +7,37 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
+	"time"
 )
 
 //Read file
 func CSVfile(f string) {
+
+}
+
+var wg sync.WaitGroup
+
+func main() {
+	var file string
+	flag.StringVar(&file, "file", "problems.csv", "set csv file")
+	flag.Parse()
+
 	//Open file
-	file, err := os.Open(f)
+	csvfile, err := os.Open(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 	//Close file
-	defer file.Close()
+	defer csvfile.Close()
 
-	r := csv.NewReader(file)
+	r := csv.NewReader(csvfile)
 
-	ansCorrect := 0
-	totalQuestions := 0
+	correct := 0
+	questions := 0
 
-	//Set timer
-
+	timer := time.NewTimer(5 * time.Second)
+	ansChannel, q := make(chan string)
 	for {
 		line, err := r.Read()
 		if err == io.EOF {
@@ -34,35 +46,36 @@ func CSVfile(f string) {
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		fmt.Printf("Question %v: %v = ", totalQuestions+1, line[0])
+		fmt.Printf("Question %v: %v = ", questions+1, line[0])
 
 		var answer string
 		fmt.Scan(&answer)
 
-		if answer == line[1] {
-			ansCorrect++
+		select {
+		case ansChannel <- answer:
+			if answer == line[1] {
+				correct++
+			}
+		case <-timer.C:
+			break
 		}
 
-		totalQuestions++
-
+		questions++
 	}
 
-	fmt.Printf("Math Quiz! Your score was: %v/%v!\n", ansCorrect, totalQuestions)
-
-}
-
-func main() {
-	var file string
-	flag.StringVar(&file, "file", "problems.csv", "set csv file")
-	flag.Parse()
-
-	//Function to Read File
-	CSVfile(file)
+	fmt.Printf("Math Quiz! Your score was: %v/%v!\n", correct, questions)
 
 }
 
 /*
+Part 1
+-> Topic: csvs topic: flags topic: opening files topic: strings
+- Programa que lê um arquivo CSV
+- Flag onde o usuário pode modificar o arquivo
+- Analisar quantas questões estão certas e o total
+- Arquivo com a primeira coluna de pergunta e segunda de resposta na mesma linha
+Part 2
+-> Topic: goroutines topic: channels topic: timers
 - Adicionar um timer de 30 segundos e encerrar se não responder.
 - Apertar Enter antes de começar o tempo a correr
 - Mesmo que a resposta seja errada, passa para a próxima pergunta
